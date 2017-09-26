dffb4eb977f9493ec90662c63b9f95c60163a1cc
Artie Keith <akeith047@gmail.com>
2017-09-26 18:43:48 -0500

    Exercise 18 and some updates from my assignment 2 ps assignment 2 isnt ready yet

--- Assignment3/portfolio/src/script/scripts.js
+++ Assignment3/portfolio/src/script/scripts.js
@@ -5,10 +5,6 @@ var quotes = [
     "'I believe the most meaningful way to succeed is to help other people succeed' - Adam M Grant",
     "'No man has a good enough memory to be a successful liar' - Abraham Lincoln",
     "'If you could kick the person in the pants responsible for most of your trouble, you wouldn't sit for a month' - Theodore (Teddy) Roosevelt",
-    "Random6",
-    "Random7",
-    "Random8",
-    "Random9",
 ]
 
 var getRandomNumber = Math.floor(Math.random() * quotes.length);
